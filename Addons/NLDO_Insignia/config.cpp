ea775e305a93490b96da34a7a8ec8f7944ce210f
Ancor Baumhöer <ancorb1974@live.nl>
2015-09-15 10:54:25 +0200

    added map structure
    added maps

---
+++ Addons/NLDO_Insignia/config.cpp
@@ -0,0 +1,26 @@
+class CfgPatches {
+	class NLDO_Units_Insignia {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"NLDO_Units_Infantry"};
+	};
+};
+
+class CfgUnitInsignia
+{
+    class NLDO_76AMB
+    {
+        displayName = "[NLDO] 76AMB";
+        author = "Lowlands Tactical";
+        texture = "\NLD_Infantry_Units\Badges\76th_Airborne_Division_Russia.paa";
+        textureVehicle = "\NLD_Infantry_Units\Badges\76th_Airborne_Division_Russia.paa";
+    };
+    class NLDO_76AMB_Rondel
+    {
+        displayName = "[NLDO] 76AMB (Rondel)";
+        author = "Lowlands Tactical";
+        texture = "\NLDO_Infantry_Units\Badges\76th_Airborne_Division_Rondel.paa";
+        textureVehicle = "\NLDO_Infantry_Units\Badges\76th_Airborne_Division_Rondel.paa";
+    };
+};
