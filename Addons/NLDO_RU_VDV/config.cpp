dfbe04e01e5b0e86a7ffd9be3a4492a1ab39e901
ancorb1974 <ancorb1974@live.nl>
2016-02-03 22:32:20 +0100

    added config for factionclasses

---
+++ Addons/NLDO_RU_VDV/config.cpp
@@ -0,0 +1,27 @@
+class CfgPatches {
+	class NLDO_RU_VDV_Main {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Modules_F_Curator"};
+	};
+};
+
+class CfgFactionClasses
+{
+
+	class NLDO_RU_VDV_76
+	{
+		displayName="[NLDO] 76th Guards AA";
+		icon="\NLDO_RU_VDV\Icon\Icon.jpg";
+		priority=1;
+		side=0;
+	};
+	class NLDO_RU_VDV_31
+	{
+		displayName="[NLDO] 31st Guards AA";
+		icon="\NLDO_RU_VDV\Icon\Icon.jpg";
+		priority=1;
+		side=0;
+	};
+};
