6f10a1e224ffb6f6a8c39e9d8314486ca3364c60
Sacha Ligthert <sacha@ligthert.net>
2016-02-06 04:08:21 +0100

    Fixes that work for now.

--- Addons/x_makepbo.bat
+++ Addons/x_makepbo.bat
@@ -4,4 +4,5 @@ REM cleanup
 DEL /S /Q *.pbo
 
 "C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -UP NLDO_Common
-"C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -U NLDO_RU_VDV_31
+"C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -UP NLDO_RU_VDV_31
+"C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -UP SP_Pack
