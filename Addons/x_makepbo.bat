5061079b5d17de58c54bdddc6b7b4059c583814b
ShiftySean <Sean_wijntjes@hotmail.com>
2016-03-12 14:27:13 +0100

    test

--- Addons/x_makepbo.bat
+++ Addons/x_makepbo.bat
@@ -5,7 +5,7 @@ DEL /S /Q *.pbo
 
 "C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -UP SP_Pack
 "C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -UP NLDO_Common
-"C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -UP NLDO_RU_VDV_31
+"C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -U NLDO_RU_VDV_31
 "C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -UP NLDO_RU_VDV_76
 "C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -UP NLDO_UA_UAF_BG
 "C:\Program Files (x86)\Mikero\DePboTools\bin\MakePbo.exe" -U NLDO_UA_UAF_95
